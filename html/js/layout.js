8d37171bb44724088899d91fd7f06c3e919ea1ae
Mark Mandel <markmandel@google.com>
2016-04-08 14:55:08 -0700

    Basic layout, with expansion.

---
+++ html/js/layout.js
@@ -0,0 +1,37 @@
+/*
+ *  Copyright 2016 Google Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License")
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict";
+
+/*
+ * Deals with layout
+ */
+var Layout = (function() {
+
+    //public functions
+    return {
+        fillVertically: function() {
+            //fill vertically
+            var header = document.querySelector("header");
+            var chat = document.querySelector("#chat");
+            chat.style.height = (window.innerHeight - header.clientHeight - 32) + "px";
+
+            //expand out chat area
+
+
+        }
+    }
+})();
