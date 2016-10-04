d55d857a31dcf3b17cad70865a6f7958a34dfae1
Mark Mandel <markmandel@google.com>
2016-10-04 13:42:41 -0700

    Simplify google session login.

--- html/js/session.js
+++ html/js/session.js
@@ -78,13 +78,7 @@ var Session = (function() {
         provider.addScope("https://www.googleapis.com/auth/userinfo.email");
         provider.addScope("https://www.googleapis.com/auth/userinfo.profile");
 
-        firebase.auth().signInWithPopup(provider).then(function(result) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = result.credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-
-        }).catch(function(error) {
+        firebase.auth().signInWithPopup(provider).catch(function(error) {
             console.log("Google Login error: ", error);
         });
     }
